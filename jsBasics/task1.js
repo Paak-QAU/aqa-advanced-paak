2e499e4cecfbf325285e4c05e718a29aa333b18b
Oleksandr Sydorenko <alxn.sydorenko@gmail.com>
2025-03-07 19:39:39 +0200

    Add jsBasics folder for JavaScript tasks

---
+++ jsBasics/task1.js
@@ -0,0 +1,8 @@
+// Завдання 1: Оголошення змінних для примітивних типів
+const str = "Привіт, світ!";
+const num = 42;
+const bool = true;
+const empty = null;
+let undef;
+
+console.log(str, num, bool, empty, undef);
