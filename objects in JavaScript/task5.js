46cb7dc80087802c801e61eed1eb844b647e0b5d
Oleksandr Sydorenko <alxn.sydorenko@gmail.com>
2025-03-25 14:27:04 +0200

    objects in JavaScript

--- objects in JavaScript/task5.js
+++ objects in JavaScript/task5.js
@@ -4,6 +4,13 @@ const users = [
     { name: "Анастасія", email: "nastya@mail.com", age: 25 },
     { name: "Максим", email: "max@mail.com", age: 27 }
 ];
+
+console.log("З використанням деструктуризації:");
 for (const { name, email, age } of users) {
     console.log(`Ім'я: ${name}, Email: ${email}, Вік: ${age}`);
+}
+
+console.log("Без використання деструктуризації:");
+for (const user of users) {
+    console.log(`Ім'я: ${user.name}, Email: ${user.email}, Вік: ${user.age}`);
 }
